ed5e7d50cf08ceb05664a15593c6302f2ee76cb3
Yashin Al Fauzy <yashinfauzy@apps.ipb.ac.id>
2025-04-28 22:13:35 +0700

    yes

--- src/layout/AppHeader.tsx
+++ src/layout/AppHeader.tsx
@@ -3,7 +3,7 @@ import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router";
 import { useSidebar } from "../context/SidebarContext";
 import { ThemeToggleButton } from "../components/common/ThemeToggleButton";
-import NotificationDropdown from "../components/header/NotificationDropdown";
+// import NotificationDropdown from "../components/header/NotificationDropdown";
 import UserDropdown from "../components/header/UserDropdown";
 
 const AppHeader: React.FC = () => {
